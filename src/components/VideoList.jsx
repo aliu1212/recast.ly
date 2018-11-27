4a0daa06e124ec9aa7e0e4cb5d82eaacfa3e8e20
aliu1212 <alexliu12@gmail.com>
2018-11-26 20:40:08 -0800

    completed video list

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,19 +1,24 @@
-var VideoList = () => (
+// import App from './components/App.js';
+import VideoListEntry from '../components/VideoListEntry.js';
+
+let VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {/* {console.log('videolist props', props.vids)} */}
+    {props.vids.map(item =>
+      <VideoListEntry item={item} />
+    )}
   </div>
 );
 
+
+
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
-VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
-};
+// VideoList.propTypes = {
+//   videos: React.PropTypes.array.isRequired
+// };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
+
 export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import exampleVideoData from '../src/data/exampleVideoData.js';
 
+// console.log('example video data: ', exampleVideoData)
+ReactDOM.render(<App exampleVids={exampleVideoData}/>, document.getElementById("app"));
